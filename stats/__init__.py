4e9ec96900ef634983545db69ace4b2870d39f38
cyocei <guleyroblox@gmail.com>
2025-03-27 05:10:04 +1000

    Update __init__.py

--- stats/__init__.py
+++ stats/__init__.py
@@ -1,9 +1,2 @@
-from .keser import search_username, process_brute_force_duckduckgo
 from .stats import Stats
-__version__ = "1.0.3"
-__ye__ = "vbiskit"
-
-__all__ = [
-    "search_username",
-    "process_brute_force_duckduckgo"
-] 
+__all__ = ['Stats'] 
