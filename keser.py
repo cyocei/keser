09ee187453f217e98fa1301cd3a31e0ede8d6d65
cyocei <guleyroblox@gmail.com>
2025-03-28 03:17:11 +1000

    Update keser.py

--- keser.py
+++ keser.py
@@ -17,7 +17,7 @@ import asyncio
 import io
 import socket
 from aiohttp import TCPConnector
-from stats import stats
+from stats.stats import stats
 
 def rgb(r, g, b, text):
     return f'\033[38;2;{r};{g};{b}m{text}\033[0m'
